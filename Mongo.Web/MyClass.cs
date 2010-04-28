0efb1c1135cba2d4715958d3635c2be797abe016
unknown <scorder@.WS.WSFGRP.NET>
2010-04-28 17:23:31 -0400

    Starting a membership provider.

---
+++ Mongo.Web/MyClass.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Mongo.Web
+{
+    /// <summary>
+    /// Description of MyClass.
+    /// </summary>
+    public class MyClass
+    {
+        
+    }
+}
---
+++ MongoDB.Web.Tests/Security/MongoDBMembershipProviderTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Web.Security;
+
+using NUnit.Framework;
+
+using MongoDB;
+
+namespace MongoDB.Web.Security
+{
+    [TestFixture]
+    public class MongoDBMembershipProviderTests
+    {
+        MongoDBMembershipProvider provider;
+        Mongo mongo;
+        IMongoCollection membership;
+        
+        [Test]
+        public void CreateAUser()
+        {
+            MembershipCreateStatus status;
+            provider.CreateUser("test", "pass", "test@test", "test?", "test!", true, Oid.NewOid(), out status);
+            Assert.AreEqual(MembershipCreateStatus.Success, status);
+            
+        }
+        
+        [TestFixtureSetUp]
+        public void Setup(){
+            provider = (MongoDBMembershipProvider)Membership.Provider;
+            mongo = new Mongo();
+            mongo.Connect();
+            membership = mongo["providertests"]["membership"];
+            
+            CleanUp();
+        }
+        
+        public void CleanUp(){
+            membership.Delete(new Document());
+        }
+    }
+}
